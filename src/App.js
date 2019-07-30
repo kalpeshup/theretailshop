1c66a98f8a116a56878814f4a6b4365b18473501
kalpeshup <kalpesh.patil@gmail.com>
2019-07-30 20:26:47 +0530

    separated home, directory and menu components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import HomePage from './pages/homepage.component';
 
 function App() {
   return (
     <div className="App">
-      <p>This is my first react app</p>
+      <HomePage/>
     </div>
   );
 }
